bd42670d8d7975693ea39f718136cf82c6e19074
Gokul <146622216+0smic@users.noreply.github.com>
2023-12-17 04:56:14 +0530

    Create display.c

---
+++ src/display.c
@@ -0,0 +1,29 @@
+// Copyright (c) 2023 Gokul B
+// Distributed under the MIT/X11 software license, see the accompanying
+// http://www.opensource.org/licenses/mit-license.php.
+
+#include <stdio.h>
+#include "display.h"
+
+int start_design() {
+    printf("  _______  _______  _______  _______  _________  _________\n");
+    printf(" |  __   ||  __   ||       ||  _____||___   ___||___   ___|\n");
+    printf(" | |__)  || |__|  ||   _   || |_____     | |        | |\n");
+    printf(" |  _____||      / |  |_|  ||  _____|    | |        | |\n");
+    printf(" | |      |  |\\  \\ |       || |       ___| |___     | |\n");
+    printf(" |_|      |__| \\__\\|_______||_|      |_________|    |_|\n");
+
+    printf("\t  _______  _______  _______  __   ___\n");
+    printf("\t |   _   ||       ||       ||  | /  /\n");
+    printf("\t |  |_)  ||   _   ||   _   ||  |/  /\n");
+    printf("\t |   _   ||  |_|  ||  |_|  ||  |  /\n");
+    printf("\t |  |_)  ||       ||       ||  |\\  \\ \n");
+    printf("\t |_______||_______||_______||__| \\__\\\n");
+    printf("\n");
+    printf("\t\t***********************\n");
+    printf("\t\t*   By:  GOKUL B      *\n");
+    printf("\t\t***********************\n");
+    printf("\n");
+
+    return 0;
+}
